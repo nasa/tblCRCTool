54a6b4c981f8647cc1fb34a202f199400e946924
Dylan <dylan.z.baker@nasa.gov>
2023-12-27 09:35:08 -0500

    Fix #79, updating tblCRCTool to use new versioning system.

--- cfe_ts_crc.c
+++ cfe_ts_crc.c
@@ -42,6 +42,8 @@
 
 #include "cfe_ts_crc_version.h"
 
+#include "cfe_version.h"
+
 /* These headers are needed for CFE_FS_Header_t and CFE_TBL_File_Hdr_t, respectively.
  * This uses the OSAL definition of fixed-width types, even thought this tool
  * is not using OSAL itself.
@@ -108,11 +110,14 @@ int main(int argc, char **argv)
     int     fd;
     char    buffer[100];
     off_t   offsetReturn = 0;
+    char    VersionString[CFE_TS_CRC_CFG_MAX_VERSION_STR_LEN];
 
     /* check for valid input */
     if ((argc != 2) || (strncmp(argv[1], "--help", 100) == 0))
     {
-        printf("%s\n", CFE_TS_CRC_VERSION_STRING);
+        CFE_Config_GetVersionString(VersionString, CFE_TS_CRC_CFG_MAX_VERSION_STR_LEN, "tblCRCTool",
+            CFE_TS_CRC_VERSION, CFE_TS_CRC_BUILD_CODENAME, CFE_TS_CRC_LAST_OFFICIAL);
+        printf("%s\n", VersionString);
         printf("\nUsage: cfe_ts_crc [filename]\n");
         exit(EXIT_FAILURE);
     }
